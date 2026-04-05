687ef95b44b6a8e95674c34e9a0fb30b8ecb3b21
DinkleArora <dinkle2803@gmail.com>
2026-04-05 15:29:38 +0530

    Add files via upload

---
+++ non-primitive.js
@@ -0,0 +1,22 @@
+let username = {
+    firstname: "Dinkle",
+    isLoggedin: true,
+};
+
+console.log(username);
+console.log(typeof(username));
+
+const details = {
+    firstname: "Dinkle",
+    isLoggedin: true,
+};
+
+details.firstname = "D";
+
+console.log(details.firstname);
+
+let today = new Date();
+console.log(today.getDate());
+
+let heroes = ["a", "b", "c", true]
+console.log(heroes[0]);
