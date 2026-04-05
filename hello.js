38d33ad3f742c2b43b7d6561342fd91e24a1330f
DinkleArora <dinkle2803@gmail.com>
2026-04-05 15:29:15 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,43 @@
+console.log("hello");
+let score = 102;
+let name = "Dinkle";
+let islogged = false;
+
+let teatypes = ["lemon tea", "orange tea", "oolong tea"]
+let user = {firstname: "hitesh", lastname: "choudhary"}
+let getScore = score;
+let newScore = new Number(20);
+
+console.log(getScore);
+
+let gameName = "Spiderman";
+
+gameName = "batman";
+
+console.log(gameName);
+
+const username = "Dinkle";
+
+// username = "DinkleArora";
+
+console.log(username);
+
+console.log(typeof(score));
+console.log(typeof(newScore));
+
+let myString = "hello";
+let myStringOne = "Hola";
+let str = "Dinkle";
+
+let oldGreet = myString +" "+ "Dinkle";
+console.log(oldGreet);
+
+let greetMessage = `Hello ${str} !`;
+console.log(greetMessage);
+
+let demoOne = `Value is ${2 * 2}`;
+console.log(demoOne);
+
+let sm1 = Symbol("Dinkle");
+let sm2 = Symbol("Dinkle");
+console.log(sm1 == sm2);
