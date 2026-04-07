4aff05e82f84cffc2d2d1280edf94f8297b6d32a
DinkleArora <dinkle2803@gmail.com>
2026-04-07 22:59:38 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,39 @@
+let num1 = 5;
+let num2 = 8;
+
+if(num1 > num2){
+    console.log("num1 is greater than num2");
+}
+else{
+    console.log("num2 is greater than num1");
+}
+
+str1 = "Dinkle";
+str2 = "Arora";
+
+if(str1 == str2){
+    console.log("strings are equal");
+}
+else{
+    console.log("strings are not equal");
+}
+
+let score = 44
+
+if(typeof(score) === 'number'){
+    console.log("score is a number");
+}
+else{
+    console.log("score is not a number");
+}
+
+let items = ["a"];
+
+console.log(items.length)
+
+if(items.length === 0){
+    console.log("array is empty");
+}
+else{
+    console.log("array is non empty");
+}
